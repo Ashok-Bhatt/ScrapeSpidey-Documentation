3c22710ac11be0f9967efde388662046f064f5e0
Ashok-Bhatt <ashokbhatt2048@gmail.com>
2025-09-09 17:09:52 +0530

    Add vercel.json and add localstorage functionality with apiContext

--- src/context/apiKeyContext.jsx
+++ src/context/apiKeyContext.jsx
@@ -1,14 +1,21 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const apiKeyContext = createContext();
 
 export default function ApiKeyProvider({children}){
 
     const [apiKey, setApiKey] = useState("");
-    const [apiKeyAutoAttach, setApiKeyAutoAttach] = useState(false);
+    const [apiKeyAutoAttach, setApiKeyAutoAttach] = useState((()=>{
+        if (localStorage.getItem("apiKeyAutoAttach")) return Boolean(localStorage.getItem("apiKeyAutoAttach"));
+        else return false;
+    })());
+
+    useEffect(()=>{
+        localStorage.setItem("apiKeyAutoAttach", apiKeyAutoAttach);
+    }, [apiKeyAutoAttach]);
 
     const toggleApiKeyAutoAttach = () => {
-        setApiKeyAutoAttach((prev)=>!prev);
+        setApiKeyAutoAttach(!apiKeyAutoAttach);
     }
 
     return (<apiKeyContext.Provider value={{apiKey, setApiKey, apiKeyAutoAttach, toggleApiKeyAutoAttach}}>
